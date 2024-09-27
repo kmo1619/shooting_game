824fd51da1ce13e33c5a6e6de682cb99493d901d
kmo1619 <1619kmo@naver.com>
2024-09-27 12:52:57 +0900

    item added

--- Shooting_game.cpp
+++ Shooting_game.cpp
@@ -14,6 +14,14 @@ struct BULLET {
 	bool fire;
 };
 
+//아이템 구조체
+struct ITEM {
+	int x;
+	int y;
+	int type;
+	bool exist;
+};
+
 //배열로 배경 좌표를 만든다.
 char bg[YMAX][XMAX];
 
@@ -25,8 +33,17 @@ int playerY;
 int enemyX;
 int enemyY;
 
+//아이템 좌표
+int itemX;
+int itemY;
+//아이템 타입
+int itemType;
+
 //미사일 갯수
 BULLET playerBullet[21];
+
+//아이템 갯수
+ITEM items[3];
 
 //함수 설계
 void ClearScreen(); //화면을 지운다
@@ -38,9 +55,12 @@ void PlayerDraw();	//플레이어 그리는 함수
 void EnemyDraw();	//적그리는 함수
 void EnemyMove(int *life);	//적움직이는 함수
 void ClashEnemyAndBullet(int *score, int level);	//충돌처리 함수
+void ItemCreate();
+void ItemDraw();
+void ClashItemAndPlayer();
 
 void main() {
-	int speed = 50;	//게임 속도 
+	int speed = 30;	//게임 속도 
 	int level = 1;	//게임 난이도
 	int life = 3;	//남은 목숨
 	int score = 0;	//스코어
@@ -61,6 +81,14 @@ void main() {
 		playerBullet[i].fire = false;
 	}
 
+	//아이템 초기화
+	for (int i = 0; i < 3; i++) {
+		items[i].x = 0;
+		items[i].y = 0;
+		items[i].type = 0;
+		items[i].exist = 0;
+	}
+
 	//초단위 시간을 받아옴
 	int dwTime = GetTickCount64();
 	int stTime = GetTickCount64();
@@ -75,10 +103,18 @@ void main() {
 			//그려주는 함수
 			PrintScreen(level, life, score);
 		}
-		if (stTime + 10000 < GetTickCount64()) {
+		if (stTime + 1000 < GetTickCount64()) {
 			speed = speed - 5;
 			level++;
 			stTime = GetTickCount64();
+			//아이템 생성
+			ItemCreate();
+			//지우는 함수
+			ClearScreen();
+			//플레이어나 적이 움직이는 함수
+			GameMain(&life, &score, level);
+			//그려주는 함수
+			PrintScreen(level, life, score);
 		}
 		if (life == 0) {
 			//콘솔창을 지움
@@ -116,6 +152,8 @@ void GameMain(int *life, int *score, int level)
 	EnemyDraw();
 	//미사일과 적 충돌
 	ClashEnemyAndBullet(score, level);
+	//아이템 그리기
+	ItemDraw();
 
 }
 
@@ -232,6 +270,7 @@ void EnemyMove(int *life)
 		//적을 랜덤하게 y쪽 좌표를 바꿔준다
 		enemyX = 77;
 		enemyY = (rand() % 20) + 2;
+		//목숨을 1개 없앤다
 		*life = *life - 1;
 	}
 }
@@ -250,9 +289,57 @@ void ClashEnemyAndBullet(int *score, int level)
 					enemyX = 77;
 					enemyY = (rand() % 20) + 2;
 					playerBullet[i].fire = false;
+					//스코어 상승
 					*score = *score + 100 * level;
 				}
 			}
 		}
 	}
 }
+
+void ItemCreate() {
+	for (int i = 0; i < 3; i++) {
+		//아이템이 준비되어있는 경우에만 생성
+		if (items[i].exist == false) {
+			items[i].exist = true;
+			//아이템 랜덤 생성
+			items[i].x = (rand() % 20) + 2;
+			items[i].y = (rand() % 3) + 1;
+			items[i].type = (rand() % 2);
+		}
+	}
+}
+
+void ItemDraw() {
+	//아이템 그리기
+	for (int i = 0; i < 3; i++) {
+		if (items[i].exist == true) {
+			if (items[i].type == 1) {
+				bg[items[i].x][items[i].y] = 'L';
+			}
+			else {
+				bg[items[i].x][items[i].y] = 'P';
+			}
+		}
+	}
+}
+
+void ClashItemAndPlayer()
+{
+	//아이템이 3개 판별
+	for (int i = 0; i < 3; i++) {
+		if (items[i].exist == true) {
+			//플레이어와 아이템 Y값이 +-1까지 충돌가능하게 판별
+			if (items[i].y >= (playerY - 1) && items[i].y <= (playerY + 1)) {
+				//플레이어와 아이템 X값이 +-1까지 충돌가능하게 판별
+				if (items[i].x >= (playerX - 1) && items[i].x <= (playerX + 1)) {
+					//충돌되면 아이템 획득하기
+
+					items[i].exist = false;
+					playerX = 1;
+				}
+			}
+		}
+	}
+}
+
